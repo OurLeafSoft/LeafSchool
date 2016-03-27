b0f40d096fde41184a5f8c1de944222d28024184
dinesh kumar <danishkumarmca@gmail.com>
2016-03-27 23:34:30 +0530

    [#update : dashboard controller added]

--- src/main/java/com/app/controller/DashBoardController.java
+++ src/main/java/com/app/controller/DashBoardController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.leafsoft.org.OrgUtil;
+import com.leafsoft.school.dao.DaoSelectorUtil;
+import com.leafsoft.school.dao.OrgDetailsDao;
+import com.leafsoft.school.model.OrgDetail;
 import com.leafsoft.util.JSONUtil;
 
 @Controller
@@ -21,15 +24,19 @@ public class DashBoardController {
 	
 	private static Logger LOGGER = Logger.getLogger(SchoolController.class.getName());
 	
-	@RequestMapping(produces="application/json",value = "/dashboard/sidebar", method = RequestMethod.GET)
+	@RequestMapping(produces="application/json",value = "/dashboard", method = RequestMethod.GET)
 	public @ResponseBody String getEmployees(HttpServletRequest request) 
 	{
 		JSONObject response = new JSONObject();
 		try {
 			JSONObject sidbar_json = JSONUtil.getInstance().getSidebarJSON();
+			OrgDetailsDao orgdao = DaoSelectorUtil.getOrganizationDao();
+			OrgDetail ordetails = orgdao.loadOrgDetailByOrgId(OrgUtil.getOrgId(), OrgUtil.getOwnerid());
 			JSONArray rolebased_array = sidbar_json.getJSONArray("admin");
 			response.put("code", 0);
 			response.put("sidbar_array", rolebased_array);
+			response.put("user", new JSONObject(OrgUtil.getCurrentUser()));
+			response.put("orgdetails", new JSONObject(ordetails));
 		
 		} catch(Exception e) {
 			LOGGER.log(Level.SEVERE, e.getMessage(), e);
